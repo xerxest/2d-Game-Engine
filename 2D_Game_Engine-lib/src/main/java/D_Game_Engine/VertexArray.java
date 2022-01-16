3d444b9323c789858abe0b8191ba550ff031fb2d
Xerxes Tarman <xerxestarman@gmail.com>
2022-01-16 14:24:56 -0600

    Vertex array abstraction

--- 2D_Game_Engine-lib/src/main/java/D_Game_Engine/VertexArray.java
+++ 2D_Game_Engine-lib/src/main/java/D_Game_Engine/VertexArray.java
@@ -1,13 +1,26 @@
 package D_Game_Engine;
+import static org.lwjgl.opengl.GL33.*;
 
 public class VertexArray {
 	
 	private int ID;
 	
-	// Read More about Vertex Array Objects, multiple Vertex attributes ?? 
-	
-	public VertexArray() {
+	public VertexArray(VertexBuffer vb) {
 		
+		ID = glGenVertexArrays();
+				
+		glBindVertexArray(ID);
+		
+		vb.bind();
+		
+		//only works with floats !!
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0,2,GL_FLOAT,false,2*4,0);
+		
+	}
+	
+	public void bind(){
+		glBindVertexArray(ID);
 	}
 
 }
